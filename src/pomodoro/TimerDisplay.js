5497f3305adc3781bed01cd12b229342fb819e03
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-11-12 13:00:54 -0800

    Update to resolve last test failure.
    This commit fixes the problem with the "Session Title" - "displays "Focusing for 05:00 minutes" after focus duration is decreased session is started " test.

--- src/pomodoro/TimerDisplay.js
+++ src/pomodoro/TimerDisplay.js
@@ -1,44 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { minutesToDuration, secondsToDuration } from "../utils/duration";
 
 const TimerDisplay = ({
   isTimerRunning,
   isSessionActive,
-  sessionStop,
   sessionLabel,
   sessionTime,
   sessionTimeToGo,
 }) => {
-  const [displayTimeToGo, setDisplayTimeToGo] = useState("00:00");
-  const [sessionTitle, setSessionTitle] = useState(
-    isTimerRunning && sessionLabel && sessionTime
-      ? `${sessionLabel} for ${minutesToDuration(sessionTime)} minutes`
-      : "Focusing for 25:00 minutes"
-  );
-
-  useEffect(() => {
-    if (sessionLabel && sessionTime) {
-      setSessionTitle(
-        () => `${sessionLabel} for ${minutesToDuration(sessionTime)} minutes`
-      );
-    }
-  }, [isTimerRunning, sessionStop, sessionLabel, sessionTime]);
-
-  useEffect(() => {
-    setDisplayTimeToGo(secondsToDuration(sessionTimeToGo));
-  }, [sessionTimeToGo]);
-
   return (
-    <div className={sessionLabel ? "col" : "col hide"}>
-      {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
-      <h2 data-testid={sessionLabel ? "session-title" : ""}>{sessionTitle}</h2>
-      {/* TODO: Update message below correctly format the time remaining in the current session */}
-      <p className="lead" data-testid={sessionLabel ? "session-sub-title" : ""}>
-        {displayTimeToGo} remaining
-      </p>
-      <h1 className={!isTimerRunning && isSessionActive ? "" : "hide"}>
+    <div className="col">
+      {isSessionActive && (
+        <h2 data-testid="session-title">{`${sessionLabel} for ${minutesToDuration(
+          sessionTime
+        )} minutes`}</h2>
+      )}
+      {isSessionActive && (
+        <p className="lead" data-testid="session-sub-title">
+          {secondsToDuration(sessionTimeToGo)} remaining
+        </p>
+      )}
+      <h3 className={!isTimerRunning && isSessionActive ? "" : "hide"}>
         PAUSED
-      </h1>
+      </h3>
     </div>
   );
 };
